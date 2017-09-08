ac214c2c2c99304dc2a57ecf4a80f91ecbeba073
Stephen Tramer <sttramer@microsoft.com>
2017-09-07 17:06:15 -0700

    Change language indicating a command is in PREVIEW.

--- src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_help.py
+++ src/command_modules/azure-cli-acr/azure/cli/command_modules/acr/_help.py
@@ -237,7 +237,7 @@ helps['acr webhook get-config'] = """
     type: command
     short-summary: Get the service URI and custom headers for the webhook.
     examples:
-        - name: Get the configuration information for a webhook. 
+        - name: Get the configuration information for a webhook.
           text: >
             az acr webhook get-config -n MyWebhook -r MyRegistry
 """
